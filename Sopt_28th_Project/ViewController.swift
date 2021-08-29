20f9610aa47c5892fe63c87bd1a4c7638293dfdf
yangwon9616 <yangwon9616@gmail.com>
2021-08-29 19:14:20 +0900

    Initial Commit

---
+++ Sopt_28th_Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sopt_28th_Project
+//
+//  Created by 조양원 on 2021/08/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
